e04de0b2091c219366c3d49ca1769e6ca63a1d9a
WillYuum <liamshere98@gmail.com>
2025-03-24 22:58:42 +0200

    feat: Implement better logic for enabling and disabling component

--- src/Component.ts
+++ src/Component.ts
@@ -7,16 +7,47 @@ import { GameObject } from "./GameObject";
  */
 export class Component {
     gameObject: GameObject | undefined;
+    private _enabled: boolean = false;
+    private _isAwake: boolean = false;
 
-    constructor() {
-        //TODO: I believe it's better to have this happen when I add the component to a GameObject
-        ComponentManager.getInstance().addComponent(this);
+    constructor() {}
+
+    /** Called once when the component is first enabled. */
+    awake() {}
+
+    /** Called every time the component is enabled. */
+    onEnable() {}
+
+    /** Called every time the component is disabled. */
+    onDisable() {}
+
+    /** Called every frame. */
+    update(deltaTime: number) {}
+
+    /** Called before the component is destroyed. */
+    destroy() {
     }
 
-    awake() {
-
+    /** Getter and setter for enabling/disabling the component */
+    get enabled(): boolean {
+        return this._enabled;
     }
 
-    update(deltaTime: number) {
+    set enabled(value: boolean) {
+        if (this._enabled === value) return;
+
+        this._enabled = value;
+
+        if (value) {
+            if (!this._isAwake) {
+                this.awake();
+                this._isAwake = true;
+            }
+            ComponentManager.getInstance().addComponent(this);
+            this.onEnable();
+        } else {
+            ComponentManager.getInstance().removeComponent(this);
+            this.onDisable();
+        }
     }
 }
